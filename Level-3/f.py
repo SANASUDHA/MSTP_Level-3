5d7c672339158641ebb962b4abc5c74c57b1f0ac
SudhaSana <sudha139k@gmail.com>
2019-11-28 16:43:10 +0530

    MSTP_Level-3 files
    I uploaded Machine Learning practice concepts

---
+++ Level-3/f.py
@@ -0,0 +1,5 @@
+def student(id,name,marks):
+	print(id)
+	print(name)
+	print(marks)
+student(2,'sana',100)
---
+++ Level-3/f1.py
@@ -0,0 +1,6 @@
+def student(id,name,marks=90):
+	print(id)
+	print(name)
+	print(marks)
+	return id
+student(2,'sudha',20)
